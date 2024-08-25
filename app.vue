87b2f20b65c38816b3256444169a8f7dc1967b67
nesb01t <nesb01t@163.com>
2024-08-26 06:52:46 +0800

    update: 加快 transition 速度

--- app.vue
+++ app.vue
@@ -20,12 +20,12 @@ body {
 
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.25s ease;
+  transition: all 0.22s ease;
 }
 
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
-  transform: translateY(20px);
+  transform: translateY(18px);
 }
 </style>
